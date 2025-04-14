de7f1e8a3cbf8def41984442431bded11df9be84
Connor Bell <ronnoclleb@gmail.com>
2025-04-14 17:03:14 +0900

    cleanup

--- src/scripts/flagConversion.js
+++ src/scripts/flagConversion.js
@@ -1,3 +1,3 @@
-export function getFlagEmoji(countryCode){
-    return String.fromCodePoint(...[...countryCode.toUpperCase()].map(x=>0x1f1a5+x.charCodeAt()));
+export function getFlagEmoji(countryCode) {
+    return String.fromCodePoint(...[...countryCode.toUpperCase()].map(x => 0x1f1a5 + x.charCodeAt()));
 }
