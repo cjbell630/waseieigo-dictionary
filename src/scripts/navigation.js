fa74b0721ce3caf9cc3e0f562ba840e82d4e1c0e
Connor Bell <ronnoclleb@gmail.com>
2025-04-19 19:10:51 +0900

    move navigation to separate JS file

---
+++ src/scripts/navigation.js
@@ -0,0 +1,12 @@
+export function navigateToCard(cardId) {
+    //let prevResultsCard = document.querySelector(".details-pane .card[display='flex']");
+    let prevResultsCard = document.querySelector(".details-pane .selected");
+    console.log(prevResultsCard);
+    prevResultsCard.classList.remove("selected");
+    prevResultsCard.style.visibility = "hidden";
+    prevResultsCard.style.display = "none";
+    let newResultsCard = document.querySelector(`#${cardId}-results`);
+    newResultsCard.classList.add("selected");
+    newResultsCard.style.visibility = "visible";
+    newResultsCard.style.display = "flex";
+}
