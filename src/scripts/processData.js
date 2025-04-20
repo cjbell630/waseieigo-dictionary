b0172303f250bafee955d9eaecb1ecf1c96a6736
Connor Bell <ronnoclleb@gmail.com>
2025-04-20 16:11:31 +0900

    fix issues with processing dictionary

--- src/scripts/processData.js
+++ src/scripts/processData.js
@@ -1,44 +1,98 @@
-export function processData(word, entry) {
-    console.log(`processing ${word}`)
-    let printKatakana = word.split('-')[0];
-    let aliasString = entry.aliases?.length > 0 ? ` (${entry.aliases.join(", ")})` : "" ?? "";
-    let numDistinctTerms = 0;
-    let allTerms = Object.values(entry.facets).flatMap(
-        (facetRegions) => {
-            numDistinctTerms++;
-            return facetRegions.flatMap(regionInfo => {
+function getAllTermsOrigin(origin, facets) {
+    let numDistinctTerms = 1;
+    let originFlags = [];
+    let originIsValid = false;
+    let originIsGeneral = false;
+    let allTerms = Object.entries(facets).flatMap(
+        ([facetName, facetInfo]) => {
+            return facetInfo.flatMap(regionInfo => {
+                if (regionInfo.terms.includes(origin)) {
+                    originFlags = regionInfo.regions ?? [];
+                    originIsValid = true;
+                    originIsGeneral = facetName === "general";
+                } else {
+                    numDistinctTerms++;
+                }
                 return regionInfo.terms
             })
         }
     );
-    console.log(allTerms)
-    let generalTerm = entry.facets.general?.at(0)?.terms?.join(", ") ?? undefined;
-    let originTerm, originMeaning,originFlags, originEval;
+    /*if (!allTerms.includes(origin)) {
+        numDistinctTerms++;
+    }*/
+    return {allTerms, numDistinctTerms, originFlags, originIsValid, originIsGeneral};
+}
+
+function getAllTermsDirect(direct, facets) {
+    let numDistinctTerms = 1;
+    let allTerms = Object.values(facets).flatMap(
+        (facetInfo) => {
+            return facetInfo.flatMap(regionInfo => {
+                if (!regionInfo.terms.includes(direct)) {
+                    numDistinctTerms++;
+                }
+                return regionInfo.terms
+            })
+        }
+    );
+    /*
+if (!allTerms.includes(direct)) {
+numDistinctTerms++;
+}*/
+    return {allTerms, numDistinctTerms};
+}
+
+export function processData(word, entry) {
+    console.log(`processing ${word}`)
+    let printKatakana = word.split('-')[0];
+    let aliasString = entry.aliases?.length > 0 ? ` (${entry.aliases.join(", ")})` : "" ?? "";
+
+    let allTerms, numDistinctTerms, originTerm, originMeaning, originFlags, originEval;
     if (entry.direct) { // if the direct translation is included
         originTerm = entry.direct.word;
+        ({
+            allTerms, numDistinctTerms
+        } = getAllTermsDirect(originTerm, entry.facets));
         originFlags = [];
         originEval = "bad";
         originMeaning = entry.direct.meaning;
     } else {
         originTerm = entry.origins[0].source;
-        originFlags = entry.origins[0].language === "" ? [] : [entry.origins[0].language];
-        originEval = originTerm === generalTerm ? "best" : allTerms.includes(originTerm) ? "good" : "bad";
+        let originIsValid, originIsGeneral;
+        ({
+            allTerms, numDistinctTerms, originFlags, originIsValid, originIsGeneral
+        } = getAllTermsOrigin(originTerm, entry.facets));
+        if (originFlags.length === 0 && entry.origins[0].language) {
+            originFlags = [entry.origins[0].language];
+            originEval = "bad";
+        } else {
+            originEval = originIsValid ? (
+                /* TODO this is where you'd flag origins as sankaku */
+                originIsGeneral && (originFlags.length === 0 || originFlags.includes("us")) ? "best" : "good"
+            ) : "bad";
+        }
     }
 
+    console.log(allTerms)
 
-    let correctionNeeded = !["good", "best"].includes(originEval);
+
+    console.log(`origin eval: ${originEval}`)
+    let correctionNotNeeded = originEval === "best" || numDistinctTerms === 1;
     let correction, correctionEval;
     let correctionFlags = [];
-    if (correctionNeeded) {
-        if (generalTerm === undefined) {
+    if (!correctionNotNeeded) {
+        console.log(`generating correction`)
+        let firstGeneralTerm = entry.facets.general?.at(0)?.terms?.join(", ") ?? undefined;
+        if (firstGeneralTerm === undefined) { // if no "best" term
             correction = allTerms[1]; // TODO maybe wont always work
             console.log(`correction thingy: ${allTerms}`)
             correctionEval = "good"; // TODO is this right?
             // TODO flag
         } else {
-            correction = generalTerm;
-            if (entry.facets.general[0].regions?.length > 0) {
-                correctionFlags = entry.facets.general[0].regions;
+            let generalIndex = firstGeneralTerm === originTerm ? 1 : 0;
+            correction = entry.facets.general[generalIndex].terms?.join(", ") ?? undefined;
+            if (entry.facets.general[generalIndex].regions?.length > 0) {
+                correctionFlags = entry.facets.general[generalIndex].regions;
                 correctionEval = "good";
             } else {
                 correctionEval = "best";
@@ -48,17 +102,17 @@ export function processData(word, entry) {
     let hasNotes = entry.notes.length > 0;
     let hasSources = entry.sources && Object.values(entry.sources).length > 0;
     console.log(`done processing ${word}`)
+    console.log(`num distinct terms for ${word}: ${numDistinctTerms}`)
     return {
         printKatakana,
         aliasString,
         numDistinctTerms,
         allTerms,
-        generalTerm,
         originTerm,
         originMeaning,
         originFlags,
         originEval,
-        correctionNeeded,
+        correctionNotNeeded,
         correction,
         correctionFlags,
         correctionEval,
