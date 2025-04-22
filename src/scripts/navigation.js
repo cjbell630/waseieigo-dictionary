24b9de3ebb003e607540d7c7192c31af1ead7932
Connor Bell <ronnoclleb@gmail.com>
2025-04-22 17:26:03 +0900

    responds to url params!!!

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -1,4 +1,4 @@
-export function navigateToCard(cardId) {
+function navigateToCardParams(cardId, newVisibility, newDisplay, newFlex, updateURL, urlData = {}, newUrl = "/") {
     //let prevResultsCard = document.querySelector(".details-pane .card[display='flex']");
     let prevResultsCard = document.querySelector(".details-pane .selected");
     console.log(prevResultsCard);
@@ -7,9 +7,27 @@ export function navigateToCard(cardId) {
     prevResultsCard.style.display = "none";
     let newResultsCard = document.querySelector(`#${cardId}-results`);
     newResultsCard.classList.add("selected");
-    newResultsCard.style.visibility = "visible";
-    newResultsCard.style.display = "flex";
+    newResultsCard.style.visibility = newVisibility;
+    newResultsCard.style.display = newDisplay;
 
     // TODO for now we're doing this
-    document.documentElement.style.setProperty('--test', '0');
+    document.documentElement.style.setProperty('--test', newFlex);
+    if (updateURL) {
+        //document.getElementById("content").innerHTML = response.html;
+        //document.title = response.pageTitle;
+        //window.history.pushState({"html":response.html,"pageTitle":response.pageTitle},"", urlPath);
+        window.history.pushState(urlData, "", newUrl);
+    }
+}
+
+export function navigateToCard(cardId, updateURL = true) {
+    if (cardId === "placeholder") {
+        navigateToCardParams(cardId, "", "", "4", updateURL);
+    } else {
+        navigateToCardParams(
+            cardId, "visible", "flex",
+            "0", updateURL, {"term": cardId},
+            `/?term=${cardId}`
+        );
+    }
 }
