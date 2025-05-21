41d69e320c7414d4ad0b36608b22de99474d89c0
Connor Bell <ronnoclleb@gmail.com>
2025-05-21 16:56:25 +0900

    preserve scroll state and search term on history navigation

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -1,4 +1,7 @@
-function navigateToCardParams(cardId, newVisibility, newDisplay, newFlex, updateURL, urlData = {}, newUrl = "/") {
+function navigateToCardParams(cardId, newVisibility, newDisplay, newFlex, updateURL, urlData = {}, newUrl = "/", query = null, scrollValue = null) {
+
+    let currentScroll = document.body.scrollTop;
+    let currentQuery = document.querySelector("#search-bar")?.value;
     //let prevResultsCard = document.querySelector(".details-pane .card[display='flex']");
     let prevResultsCard = document.querySelector(".details-pane .selected");
     console.log(prevResultsCard);
@@ -13,21 +16,33 @@ function navigateToCardParams(cardId, newVisibility, newDisplay, newFlex, update
     // TODO for now we're doing this
     document.documentElement.style.setProperty('--card-open', newFlex[0]);
     document.documentElement.style.setProperty('--card-open-word', newFlex[1]);
+    if (scrollValue) {
+        document.body.scrollTop = scrollValue;
+    }
+
+    if (query) {
+        console.log(`query found: ${query}`)
+    }
+
     if (updateURL) {
         //document.getElementById("content").innerHTML = response.html;
         //document.title = response.pageTitle;
         //window.history.pushState({"html":response.html,"pageTitle":response.pageTitle},"", urlPath);
+        console.log(`updating state to`)
+        console.log({...urlData, "query": currentQuery, "scroll": currentScroll});
+        window.history.replaceState({...window.history.state, "query": currentQuery, "scroll": currentScroll}, "");
         window.history.pushState(urlData, "", newUrl);
     }
 }
 
-export function navigateToCard(cardId, updateURL = true) {
+export function navigateToCard(cardId, updateURL = true, query = null, scrollValue = null) {
     if (cardId === "placeholder") {
-        navigateToCardParams(cardId, "", "", ["4",""], updateURL);
+        console.log(`navigating with query ${query} and scroll ${scrollValue}`)
+        navigateToCardParams(cardId, "", "", ["4", ""], updateURL, undefined, undefined, query, scrollValue);
     } else {
         navigateToCardParams(
             cardId, "visible", "flex",
-            ["0","none"], updateURL, {"term": cardId},
+            ["0", "none"], updateURL, {"term": cardId},
             `/?term=${cardId}`
         );
     }
